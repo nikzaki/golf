46449c88772cd6ff05d6e1d07fc128339e17d4fe
tejas-tagline <tejas.tagline@gmail.com>
2021-07-24 13:07:22 +0530

    Sponsor validation for all international phone number.

--- src/app/_shared_components/dynamic-form/fields.ts
+++ src/app/_shared_components/dynamic-form/fields.ts
@@ -10,7 +10,7 @@ const template_ref = {
         controlType: "text",
         required: true,
         autofillOff: true,
-        pattern: /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[789]\d{9}$/,
+        pattern: '(([+][(]?[0-9]{1,3}[)]?)|([(]?[0-9]{4}[)]?))\s*[)]?[-\s\.]?[(]?[0-9]{1,3}[)]?([-\s\.]?[0-9]{3})([-\s\.]?[0-9]{3,4})',
     },
     description: {
         controlType: "textarea",
